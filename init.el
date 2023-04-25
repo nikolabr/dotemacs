9a073e26420a8f78ac83102c41205943985374c7
nikolabr <nb91605@student.uni-lj.si>
2023-04-25 10:58:08 +0200

    Add JS mode hook

--- init.el
+++ init.el
@@ -65,6 +65,7 @@
 (which-key-mode)
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
+(add-hook 'js-mode-hook 'lsp)
 
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
